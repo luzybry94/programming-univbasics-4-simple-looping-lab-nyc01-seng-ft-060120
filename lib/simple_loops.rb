3747c2da20e025f1bded81fc6fd01333d4db5de8
Chaim Bryski <luzybry94@gmail.com>
2020-05-05 22:04:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,20 @@ def output_array(array)
     count += 1
   end
 end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    array[count].to_s << new_array
+    count += 1
+  end
+  new_array
+end
+    
+end
+
+
+
+
+
